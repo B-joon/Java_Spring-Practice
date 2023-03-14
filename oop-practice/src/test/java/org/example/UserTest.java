9443ad27954aeeff3959a9bcfa4df32bfd0fadbc
B-joon <dosel45@gmail.com>
2023-03-14 14:54:13 +0900

    Part1. 나만의 MVC 프레임워크 만들기 Ch03. 객체지향 패러다임 01. 테스트코드 실습

---
+++ oop-practice/src/test/java/org/example/UserTest.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class UserTest {
+
+    @DisplayName("패스워드를 초기화한다.")
+    @Test
+    void passwordTest() {
+        // given
+        User user = new User();
+
+        // when
+        // 메서드가 하나인 FunctionalInterface인 경우 구현체를 만들 필요 없이
+        // 람다를 사용하면 된다. (31줄 참고)
+        // FunctionalInterface : 메서드가 하나인 인터페이스
+        user.initPassword(() -> "abcdefgh");
+
+        // then
+        assertThat(user.getPassword()).isNotNull();
+    }
+    @DisplayName("패스워드가 요구사항에 부합되지 않아 초기화가 되지 않는다.")
+    @Test
+    void passwordTest2() {
+        // given
+        User user = new User();
+
+        // when
+        user.initPassword(new WrongFixedPasswordGenerator());
+
+        // then
+        assertThat(user.getPassword()).isNull();
+    }
+}
